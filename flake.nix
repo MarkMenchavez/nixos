a9ad8bf0f4ced4b3b5392fc7dd278440714329aa
Mark Menchavez <markchristian.menchavez@gmail.com>
2026-07-15 12:07:11 +0800

    Fix Typos

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
                         home-manager.useUserPackages = true;
                         home-manager.users.mcdm =
                             import ./users/mcdm-home.nix;
-                    }
+                    };
                 ];
             };
     };

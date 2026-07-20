016fd4d0a77a95cbadb91305ff19292ea7695837
Mark Menchavez <markchristian.menchavez@gmail.com>
2026-07-20 12:11:02 +0800

    SwayFx nix

--- users/mcdm-home-swayfx.nix
+++ users/mcdm-home-swayfx.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
     imports = [
@@ -14,6 +14,16 @@
 		config = {
 			modifier = "Mod4";
 
+			keybindings = lib.mkOptionDefault {
+				"Mod4+shift+b      " = "exec ${pkgs.brave}/bin/brave --incognito";
+				"Mod4+shift+return " = ''exec noctalia-shell ipc call launcher toggle'';
+				"Mod4+comma        " = ''exec noctalia-shell ipc call settings toggle'';
+				"Mod4+period       " = ''exec noctalia-shell ipc call controlCenter toggle'';
+				"Mod4+shift+r      " = ''exec noctalia-shell ipc call wallpaper random all'';
+				"Mod4+shift+w      " = ''exec noctalia-shell ipc call wallpaper toggle'';
+				"Mod4+shift+escape " = ''exec noctalia-shell ipc call lockScreen lock'';
+			};
+
 			gaps = {
 				inner = 10;
 				outer = 5;
@@ -22,20 +32,20 @@
 			bars = [ ];
 
 	        output = {
-				"Virtual-1" = {
-					mode = "2048x1152@60Hz";
-                	pos  = "0 0";
-				};
+			"Virtual-1" = {
+				mode = "2048x1152@60Hz";
+                		pos  = "0 0";
 			};
-
-			startup = [
-				{ command = "noctalia-shell"; }
-			];
 		};
+
+		startup = [
+			{ command = "noctalia-shell"; }
+		];
+	};
 	
-		extraConfig = ''
-			default_border pixel 1
-			default_floating_border pixel 1
+	extraConfig = ''
+		default_border pixel 1
+		default_floating_border pixel 1
 		'';
     };
 }

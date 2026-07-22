2391f3f510de256d086537bc9b8c38b330f55623
Mark Menchavez <markchristian.menchavez@gmail.com>
2026-07-22 13:18:32 +0800

    Add swap

--- modules/system/performance.nix
+++ modules/system/performance.nix
@@ -2,8 +2,25 @@
 
 {
     boot.kernel.sysctl = {
-        "vm.swappiness" = 10;
+        "vm.swappiness" = 180;
         "vm.dirty_ratio" = 10;
         "vm.dirty_background_ratio" = 5;
     };
+
+    zramSwap = {
+        enable = true;
+        algorithm = "zstd";
+        memoryPercent = 50;
+        priority = 100;
+    };
+
+    systemd.oomd.enable = true;
+
+    swapDevices = [
+        {
+            device = "/swapfile";
+            size = 8192;
+            priority = 0;
+        }
+    ];
 }

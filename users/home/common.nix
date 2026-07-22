100e2ecb838e467a122489667f5b76d47859c9ba
Mark Menchavez <markchristian.menchavez@gmail.com>
2026-07-22 08:50:11 +0800

    Use Dark mode (GTK)

--- users/home/common.nix
+++ users/home/common.nix
@@ -35,4 +35,24 @@
         mpv
         yazi
     ];
+
+    gtk = {
+        enable = true;
+
+        theme = {
+            name = "Adwaita-dark";
+            package = pkgs.gnome-themes-extra;
+        };
+
+        gtk3.extraConfig = {
+            gtk-application-prefer-dark-theme = 1;
+        };
+    };
+
+    dconf.settings = {
+        "org/gnome/desktop/interface" = {
+            color-scheme = "prefer-dark";
+            gtk-theme = "Adwaita-dark";
+        };
+    };
 }
